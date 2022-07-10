e4233a90eb28e5c475090f550b7225ad67703d94
Xihang Deng <xihangde@usc.edu>
2022-07-09 23:23:32 -0700

    Create 15 - Detect an edge.v

---
+++ 7 - Sequential Logic/1 - Latches & Flipflops/15 - Detect an edge.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] pedge
+);
+    reg [7:0] in_reg;
+    always@(posedge clk)begin
+    	in_reg <= in;
+        pedge <= ~in_reg&in;
+    end
+endmodule
